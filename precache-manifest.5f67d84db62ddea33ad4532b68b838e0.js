807647e176e6e6778f7afe25ba10bd8d32048b59
Ajanthi Vasantharoopan <ajanthi.v@gmail.com>
2020-05-25 12:23:09 -0400

    Updates

---
+++ precache-manifest.5f67d84db62ddea33ad4532b68b838e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12561cf6e08d878242313a66390d1bc",
+    "url": "/take-stock/index.html"
+  },
+  {
+    "revision": "b90f416754d7963f453d",
+    "url": "/take-stock/static/css/main.3d2039de.chunk.css"
+  },
+  {
+    "revision": "942ecfaf6e61c3b5c9e8",
+    "url": "/take-stock/static/js/2.451e005d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/take-stock/static/js/2.451e005d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90f416754d7963f453d",
+    "url": "/take-stock/static/js/main.1fc49fa0.chunk.js"
+  },
+  {
+    "revision": "1f775873704443d81c1a",
+    "url": "/take-stock/static/js/runtime-main.f0d1ecf1.js"
+  }
+]);
