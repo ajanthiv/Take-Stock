411754e1dad8152a0ec365330e65118c94a47cad
Ajanthi Vasantharoopan <ajanthi.v@gmail.com>
2020-05-25 15:45:08 -0400

    pull latest build

---
+++ build/precache-manifest.07f730fec343a205f41bc6d54b4efc5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f0497ba6076aa7041bb8e30b4e5ba9",
+    "url": "/Take-Stock/index.html"
+  },
+  {
+    "revision": "ca37ca92c9e1accdbff0",
+    "url": "/Take-Stock/static/css/main.3d2039de.chunk.css"
+  },
+  {
+    "revision": "942ecfaf6e61c3b5c9e8",
+    "url": "/Take-Stock/static/js/2.451e005d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Take-Stock/static/js/2.451e005d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca37ca92c9e1accdbff0",
+    "url": "/Take-Stock/static/js/main.e4a99a74.chunk.js"
+  },
+  {
+    "revision": "57eb04c45de4160357f8",
+    "url": "/Take-Stock/static/js/runtime-main.df4dac86.js"
+  }
+]);
